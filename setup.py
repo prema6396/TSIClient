308c49f6441c2c0e248ba8f41b69d263b3875ac6
prema6396 <131760681+prema6396@users.noreply.github.com>
2024-01-16 18:24:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="TSIClient",
-    packages=find_packages(),
+    packages=['TSIClient'],
     version="2.1.3",
     license="MIT",
     description="The TSIClient is a Python SDK for Microsoft Azure time series insights.",
